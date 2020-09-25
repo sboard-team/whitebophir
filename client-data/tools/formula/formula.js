3bc8ecf72674df496d245bb61596ee7c9afc6a98
Ivan <frontworker@yandex.ru>
2020-09-25 22:04:34 +0300

    Недоделанные формулы

---
+++ client-data/tools/formula/formula.js
@@ -0,0 +1,144 @@
+import * as htmlToImage from '../../js/html-to-image.js';
+
+(function () { //Code isolation
+  const xlinkNS = "http://www.w3.org/1999/xlink";
+  const input = document.createElement("div");
+  input.id = "formulaToolInput";
+  var mathlive = null;
+
+  const msg = {
+    id: null,
+    imageData: null,
+    formulaData: null,
+    x: null,
+    y: null,
+    width: null,
+    height: null,
+  };
+
+  const curInput = {
+    x: null,
+    y: null,
+  };
+
+  var active = false;
+
+  function onStart() {
+    //onstart
+  }
+
+  async function renderPNG() {
+    return htmlToImage.toPng(document.getElementById('formulaToolInput').querySelector('.ML__mathlive'), {
+      backgroundColor: '#fff',
+    });
+  }
+
+  function onQuit() {
+    console.log(curInput.x);
+    console.log(curInput.y);
+    msg.id = Tools.generateUID();
+    msg.x = curInput.x;
+    msg.y = curInput.y;
+    msg.formulaData = mathlive.$latex();
+    msg.width = document.getElementById('formulaToolInput').querySelector('.ML__mathlive').offsetWidth * 2 >> 0;
+    msg.height = document.getElementById('formulaToolInput').querySelector('.ML__mathlive').offsetHeight * 2 >> 0;
+    renderPNG().then(function (data) {
+      msg.imageData = data;
+      Tools.drawAndSend(msg, Tools.list.Formula);
+    });
+    stopEdit();
+    mathlive.$perform ( "hideVirtualKeyboard" );
+  }
+
+  function clickHandler(x, y, evt, isTouchEvent) {
+    if (evt.target === input) return;
+    curInput.x = x;
+    curInput.y = y;
+    stopEdit();
+    startEdit();
+    evt.preventDefault();
+  }
+
+  function startEdit() {
+    active = true;
+    if (!input.parentNode) board.appendChild(input);
+    var clientW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var x = curInput.x * Tools.scale - document.documentElement.scrollLeft;
+    if (x + 250 > clientW) {
+      x = Math.max(60, clientW - 260)
+    }
+    input.style.left = x + 'px';
+    input.style.top = curInput.y * Tools.scale - document.documentElement.scrollTop + 20 + 'px';
+    input.focus();
+    if (mathlive === null) {
+      mathlive = MathLive.makeMathField('formulaToolInput', {
+        smartMode: true,
+        virtualKeyboardMode: 'manual',
+
+      });
+    }
+    mathlive.$perform ( "showVirtualKeyboard" );
+  }
+
+  function stopEdit() {
+    try { input.blur(); } catch (e) { /* Internet Explorer */ }
+    active = false;
+    blur();
+  }
+
+  function blur() {
+    if (active) return;
+    input.style.top = '-1000px';
+  }
+
+  function draw(data) {
+    const img = Tools.createSVGElement("image");
+    img.id = data.id;
+    img.setAttributeNS(xlinkNS, "href", data.imageData);
+    img.x.baseVal.value = data['x'];
+    img.y.baseVal.value = data['y'];
+    img.setAttribute("width", data.width);
+    img.setAttribute("height", data.height);
+    if (msg.properties) {
+      for (var i = 0; i < msg.properties.length; i++) {
+        img.setAttribute(msg.properties[i][0], msg.properties[i][1]);
+      }
+    }
+    Tools.drawingArea.appendChild(img);
+  }
+
+  function updateText(textField, text) {
+    textField.textContent = text;
+  }
+
+  function createTextField(fieldData) {
+    var elem = Tools.createSVGElement("text");
+    elem.id = fieldData.id;
+    elem.setAttribute("x", fieldData.x);
+    elem.setAttribute("y", fieldData.y);
+    if (fieldData.properties) {
+      for (var i = 0; i < fieldData.properties.length; i++) {
+        elem.setAttribute(fieldData.properties[i][0], fieldData.properties[i][1]);
+      }
+    }
+    elem.setAttribute("font-size", fieldData.size);
+    elem.setAttribute("fill", fieldData.color);
+    elem.setAttribute("opacity", Math.max(0.1, Math.min(1, fieldData.opacity)) || 1);
+    if (fieldData.txt) elem.textContent = fieldData.txt;
+    Tools.drawingArea.appendChild(elem);
+    return elem;
+  }
+
+  Tools.add({ //The new tool
+    "name": "Formula",
+    "shortcut": "t",
+    "listeners": {
+      "press": clickHandler,
+    },
+    "onstart": onStart,
+    "onquit": onQuit,
+    "draw": draw,
+    "mouseCursor": "text"
+  });
+
+})(); //End of code isolation
