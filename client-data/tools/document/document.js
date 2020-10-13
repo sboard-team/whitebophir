1955295aa2864c0453a9573ae25a3921145be431
Ivan <frontworker@yandex.ru>
2020-10-13 16:44:51 +0300

    Изменение размеров изображения при вставке, замена jpeg на png

--- client-data/tools/document/document.js
+++ client-data/tools/document/document.js
@@ -1,6 +1,6 @@
 (function documents() { //Code isolation
     var xlinkNS = "http://www.w3.org/1999/xlink";
-    const fileTypes = ['jpeg', 'jpg', 'webp', 'png'];
+    const fileTypes = ['jpeg', 'jpg', 'webp', 'png', 'ico', 'svg'];
     function preventDefault(e) {
         e.preventDefault();
         e.stopPropagation();
@@ -16,7 +16,6 @@
             const data = e.dataTransfer;
             const file = data.files[0];
             const fileType = file.name.split('.')[file.name.split('.').length - 1].toLowerCase();
-
             if (fileTypes.includes(fileType)) {
                 var reader = new FileReader();
                 reader.readAsDataURL(file);
@@ -76,7 +75,7 @@
                 ctx.canvas.width = image.width * scale;
                 ctx.canvas.height = image.height * scale;
                 ctx.drawImage(image, 0, 0, image.width * scale, image.height * scale);
-                var dataURL = ctx.canvas.toDataURL("image/jpeg", 0.8);
+                var dataURL = ctx.canvas.toDataURL("image/png", 0.8);
 
                 // Compressed file size as data url, approximately 1/3 larger than as bytestream
                 size = dataURL.length;
@@ -88,16 +87,28 @@
                 ));
             } while (size > Tools.server_config.MAX_DOCUMENT_SIZE);
 
+            var width = this.width * scale;
+            var height = this.height * scale;
+            const aspect = width / height;
+            if (height > document.documentElement.clientHeight / 2 / Tools.scale) {
+              height = document.documentElement.clientHeight / 2 / Tools.scale;
+              width = height * aspect;
+            }
+            if (width > document.documentElement.clientWidth / 2 / Tools.scale) {
+              width = document.documentElement.clientWidth / 2 / Tools.scale;
+              height = width / aspect;
+            }
+            const offsetHeight = document.documentElement.scrollLeft === 0 ? -Tools.svg.getBoundingClientRect().left : document.documentElement.scrollLeft;
             var msg = {
                 id: uid,
                 type: "doc",
                 data: dataURL,
                 size: size,
-                w: this.width * scale || 300,
-                h: this.height * scale || 300,
-                x: (100 + document.documentElement.scrollLeft) / Tools.scale + 10,
-                y: (100 + document.documentElement.scrollTop) / Tools.scale + 10,
-                select: true
+                w: width,
+                h: height,
+                x: ((offsetHeight + document.documentElement.clientWidth / 2) / Tools.scale) - width / 2,
+                y: ((document.documentElement.scrollTop + document.documentElement.clientHeight / 2) / Tools.scale) - height / 2,
+                select: true,
                 //fileType: fileInput.files[0].type
             };
 
@@ -108,14 +119,13 @@
     };
 
     function draw(msg) {
-        var aspect = msg.w/msg.h;
         var img = Tools.createSVGElement("image");
         img.id=msg.id;
         img.setAttributeNS(xlinkNS, "href", msg.data);
         img.x.baseVal.value = msg['x'];
         img.y.baseVal.value = msg['y'];
-        img.setAttribute("width", 400*aspect);
-        img.setAttribute("height", 400);
+        img.setAttribute("width", msg.w);
+        img.setAttribute("height", msg.h);
         if (msg.properties) {
             for (var i = 0; i < msg.properties.length; i++) {
                 img.setAttribute(msg.properties[i][0], msg.properties[i][1]);
