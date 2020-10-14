f145b608429c907b55631067302dc1b487042636
Ivan <frontworker@yandex.ru>
2020-10-14 13:28:11 +0300

    Update zoom.js

--- client-data/tools/zoom/zoom.js
+++ client-data/tools/zoom/zoom.js
@@ -192,6 +192,8 @@
                     const newMoveY = lastY - evt.touches[0].clientY - evt.touches[1].clientY;
                     const newMoveX = lastX - evt.touches[0].clientX - evt.touches[1].clientX;
                     console.log('touchmove');
+                    console.log(newMoveX);
+                    console.log(newMoveY);
                     window.scrollTo(document.documentElement.scrollLeft + newMoveX, document.documentElement.scrollTop + newMoveY);
                 }
                 lastY = evt.touches[0].clientY + evt.touches[1].clientY;
