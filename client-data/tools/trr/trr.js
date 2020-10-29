649c70edb7fb502ecd8f7847090cdabbd9eb388f
Ivan <frontworker@yandex.ru>
2020-10-29 11:48:03 +0300

    Тестовое поведение инструмента трансформации

--- client-data/tools/trr/trr.js
+++ client-data/tools/trr/trr.js
@@ -1,81 +1,48 @@
 (function () {
   var moveable = null;
   var target = null;
+  var selecto = null;
+  var targets = [];
 
   function checkElementIsDraw(element) {
     return (element.id !== 'gridContainer' && element !== Tools.svg && element !== Tools.drawingArea && Tools.drawingArea.contains(element));
   }
 
   function press(x, y, evt) {
-    console.log(evt.target)
-    if (evt.target.getAttribute('class') && evt.target.classList.contains('moveable-control') || evt.target.classList.contains('moveable-area')) return;
-    if (checkElementIsDraw(evt.target)) {
-      if (evt.target === target) {
-        return;
-      } else if ((evt.ctrlKey || evt.metaKey) && moveable !== null) {
-        moveable.target = [...moveable.target, evt.target];
-        moveable.padding = {"left": 0, "top": 0, "right": 0, "bottom": 0};
-        moveable.pinchable = false;
-        moveable.scalable = false;
-        moveable.rotatable = false;
-        target = evt.target;
-        setTransformOrigin(target);
-        return;
-      } else {
-        destroy();
-      }
-      target = evt.target;
-      setTransformOrigin(target);
-      moveable = new Moveable(Tools.board, {
-        // If you want to use a group, set multiple targets(type: Array<HTMLElement | SVGElement>).
-        target: [evt.target],
-        defaultGroupOrigin: "50% 50%",
-        container: Tools.board,
-        dragArea: true,
-        draggable: true,
-        pinchable: false,
-        scalable: true,
-        rotatable: true,
-        origin: true,
-        keepRatio: true,
-        pinchThreshold: 20,
-        throttleRotate: 1,
-        throttleDrag: 0,
-        startDragRotate: 0,
-        throttleDragRotate: 0,
-        padding: {"left": 10, "top": 10, "right": 10, "bottom": 10},
-      });
-      console.log(evt.target.style.transform);
-      moveable.on("drag", (data) => {
-        target.style.transform = data.transform;
-      }).on("dragGroup", ({events}) => {
-        for (var ev of events) {
-          ev.target.style.transform = ev.transform;
-        }
-      }).on("rotateGroupStart", data => {
-        console.log(data)
-      }).on("rotateGroup", ({ events }) => {
-        events.forEach((ev, i) => {
-          console.log(ev);
-          ev.target.style.transform
-            = `translate(${ev.drag.beforeTranslate[0]}px, ${ev.drag.beforeTranslate[1]}px)`
-            + ` rotate(${ev.rotate}deg)`;
-        });
-      }).on("scaleGroup", (data) => {
-        console.log(data);
-        for (var ev of data.events) {
-          ev.target.style.transform = ev.transform;
-        }
-      }).on("pinch", (data) => {
-        target.style.transform = data.transform;
-      }).on("scale", (data) => {
-        target.style.transform = data.transform;
-      }).on("rotate", (data) => {
-        target.style.transform = data.transform;
-      });
-    } else {
-      destroy();
-    }
+	  console.log(evt.target);
+	if (evt.target.id === "gridContainer") {
+		destroyMoveable();
+		targets.map(item => item.classList.remove('selectedEl'))
+		targets.splice(0, targets.length);
+		if (selecto === null) {
+			createSelecto();
+			selecto.setSelectedTargets([])
+		}
+	} else if (!checkElementIsDraw(evt.target)){
+		destroySelecto();
+	}
+    // console.log(evt.target)
+    // if (evt.target.getAttribute('class') && evt.target.classList.contains('moveable-control') || evt.target.classList.contains('moveable-area')) return;
+    // if (checkElementIsDraw(evt.target)) {
+    //   if (evt.target === target) {
+    //     return;
+    //   } else if ((evt.ctrlKey || evt.metaKey) && moveable !== null) {
+    //     moveable.target = [...moveable.target, evt.target];
+    //     moveable.padding = {"left": 0, "top": 0, "right": 0, "bottom": 0};
+    //     moveable.pinchable = false;
+    //     moveable.scalable = false;
+    //     moveable.rotatable = false;
+    //     target = evt.target;
+    //     setTransformOrigin(target);
+    //     return;
+    //   } else {
+    //     destroy();
+    //   }
+    //   target = evt.target;
+    //   setTransformOrigin(target);
+    // } else {
+    //   destroy();
+    // }
   }
 
   function setTransformOrigin(el) {
@@ -89,12 +56,14 @@
 
 
   function onStart() {
+	  createSelecto();
     console.log('start');
   }
 
   function onQuit() {
     console.log('quit');
-    destroy();
+    destroySelecto();
+    destroyMoveable();
   }
 
   function draw(data) {
@@ -107,17 +76,97 @@
     }
   }
 
-  function destroy() {
+  function createSelecto() {
+	  console.log('createSelecto');
+	  selecto = new Selecto({
+		  // The container to add a selection element
+		  container: Tools.board,
+		  // The area to drag selection element (default: container)
+		  // Targets to select. You can register a queryselector or an Element.
+		  selectableTargets: [...document.querySelectorAll('#drawingArea>*')],
+		  // Whether to select by click (default: true)
+		  selectByClick: true,
+		  // Whether to select from the target inside (default: true)
+		  selectFromInside: false,
+		  // After the select, whether to select the next target with the selected target (deselected if the target is selected again).
+		  continueSelect: false,
+		  // Determines which key to continue selecting the next target via keydown and keyup.
+		  toggleContinueSelect: "shift",
+		  // The container for keydown and keyup events
+		  keyContainer: window,
+		  // The rate at which the target overlaps the drag area to be selected. (default: 100)
+		  hitRate: 100,
+	  });
+	  selecto.on("select", e => {
+		  e.added.forEach(el => {
+			  el.classList.add("selectedEl");
+			  setTransformOrigin(el);
+			  targets.push(el);
+		  });
+		  e.removed.forEach(el => {
+			  targets.splice(targets.findIndex(item => item === el), 1);
+			  el.classList.remove("selectedEl");
+		  });
+	  });
+  }
+
+  function destroySelecto() {
+		if (selecto !== null) {
+			console.log('destroySelecto')
+			selecto.destroy();
+			selecto = null;
+		}
+  }
+
+  function destroyMoveable() {
     if (moveable !== null) {
-      console.log('desctroy');
+	    console.log('destroyMoveable')
       moveable.destroy();
       moveable = null;
-      target = null;
     }
   }
 
   function release() {
-    console.log('release');
+  	if (targets.length > 0 && moveable === null) {
+		  console.log(targets);
+  		console.log('createMoveable');
+		  //destroySelecto();
+  		var single = targets.length === 1;
+  		var padding = single ? 10 : 0;
+		  moveable = new Moveable(Tools.board, {
+			  // If you want to use a group, set multiple targets(type: Array<HTMLElement | SVGElement>).
+			  target: targets,
+			  defaultGroupOrigin: "50% 50%",
+			  container: Tools.board,
+			  dragArea: true,
+			  draggable: true,
+			  pinchable: single,
+			  scalable: single,
+			  rotatable: single,
+			  origin: true,
+			  keepRatio: true,
+			  pinchThreshold: 20,
+			  throttleRotate: 1,
+			  throttleDrag: 1,
+			  startDragRotate: 0,
+			  throttleDragRotate: 0,
+			  padding: {"left": padding, "top": padding, "right": padding, "bottom": padding},
+		  });
+		  moveable.on("drag", (data) => {
+			  targets[0].style.transform = data.transform;
+		  }).on("dragGroup", ({events}) => {
+			  for (var ev of events) {
+				  ev.target.style.transform = ev.transform;
+			  }
+		  }).on("pinch", (data) => {
+			  targets[0].style.transform = data.transform;
+		  }).on("scale", (data) => {
+			  targets[0].style.transform = data.transform;
+		  }).on("rotate", (data) => {
+			  console.log(targets);
+			  targets[0].style.transform = data.transform;
+		  });
+    }
   }
 
   function move() {
@@ -132,6 +181,7 @@
       "move": move,
       "release": release
     },
+	  "test": targets,
     "onstart": onStart,
     "onquit": onQuit,
     "draw": draw,
